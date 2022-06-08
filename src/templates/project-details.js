ad91730d46fe4057a6f3c694fe73bbf5d2a8adf7
Znuta <user@Znuta.local>
2022-06-08 11:04:43 +0100

    initial commit

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -17,7 +17,7 @@ import { graphql, Link } from "gatsby"
 import GlobalStateProvider from "../context/provider"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import ProjectItem from "./projectItem"
+import ProjectItem from "../components/ProjectItem"
 
 const StyledSection = styled.section`
   width: 100%;
